4ffdaed57b9dc83621471fe8b28be976617af447
Prabhu-kumar95 <a.b.prabhu268@gmail.com>
2023-07-18 22:49:18 +0530

    change

--- src/NavComponents/NavItems/Nav.js
+++ src/NavComponents/NavItems/Nav.js
@@ -4,7 +4,7 @@ function NavBar(props) {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container px-4 px-lg-5">
-          <a className="navbar-brand">Applekart</a>
+          <p className="navbar-brand">Applekart</p>
           <button
             className="navbar-toggler"
             type="button"
@@ -19,15 +19,15 @@ function NavBar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page">
+                <p className="nav-link active" aria-current="page">
                   Home
-                </a>
+                </p>
               </li>
               <li className="nav-item">
-                <a className="nav-link">About</a>
+                <p className="nav-link">About</p>
               </li>
               <li className="nav-item dropdown">
-                <a
+                <p
                   className="nav-link dropdown-toggle"
                   id="navbarDropdown"
                   role="button"
@@ -35,19 +35,13 @@ function NavBar(props) {
                   aria-expanded="false"
                 >
                   Shop
-                </a>
+                </p>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <a className="dropdown-item">All Products</a>
+                    <p className="dropdown-item">All Products</p>
                   </li>
                   <li>
                     <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item">Popular Items</a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item">New Arrivals</a>
                   </li>
                 </ul>
               </li>
